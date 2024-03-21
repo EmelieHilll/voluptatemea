8b95548aff370297cc33475e7c8865240f29d0d6
EmelieHilll <qchdgxkizo@outlook.com>
2024-03-21 23:30:54 +0800

    eos

---
+++ polyfills/stringprototypecontains.js
@@ -0,0 +1,50 @@
+/*! http://mths.be/contains v0.2.0 by @mathias */
+if (!String.prototype.contains) {
+	(function() {
+		'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
+		var toString = {}.toString;
+		var defineProperty = (function() {
+			// IE 8 only supports `Object.defineProperty` on DOM elements
+			try {
+				var object = {};
+				var $defineProperty = Object.defineProperty;
+				var result = $defineProperty(object, object, object) && $defineProperty;
+			} catch(error) {}
+			return result;
+		}());
+		var indexOf = ''.indexOf;
+		var contains = function(search) {
+			if (this == null) {
+				throw TypeError();
+			}
+			var string = String(this);
+			if (search && toString.call(search) == '[object RegExp]') {
+				throw TypeError();
+			}
+			var stringLength = string.length;
+			var searchString = String(search);
+			var searchLength = searchString.length;
+			var position = arguments.length > 1 ? arguments[1] : undefined;
+			// `ToInteger`
+			var pos = position ? Number(position) : 0;
+			if (pos != pos) { // better `isNaN`
+				pos = 0;
+			}
+			var start = Math.min(Math.max(pos, 0), stringLength);
+			// Avoid the `indexOf` call if no match is possible
+			if (searchLength + start > stringLength) {
+				return false;
+			}
+			return indexOf.call(string, searchString, pos) != -1;
+		};
+		if (defineProperty) {
+			defineProperty(String.prototype, 'contains', {
+				'value': contains,
+				'configurable': true,
+				'writable': true
+			});
+		} else {
+			String.prototype.contains = contains;
+		}
+	}());
+}
